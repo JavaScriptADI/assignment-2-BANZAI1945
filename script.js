b01e2094fe9e09b79d6fe831bc803038f955751d
BANZAI1945 <shantadze99@gmail.com>
2024-09-02 12:17:23 +0400

    12

---
+++ script.js
@@ -0,0 +1,53 @@
+// 1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+
+let A = 'irakli';
+console.log(A);
+
+// 2. What is the difference between "5" and 5?
+        // "5" aris teqsti xolo 5 cifri 
+
+        
+// 3. Declare a variable and assign a number as a value. Make sure the number is greater than 9. Print the variable to the console.
+let B = 123456789;
+console.log(B);
+
+// 4. Declare a variable and assign a boolean as a value. Print the variable to the console.
+let C = true;
+console.log(C);
+
+// 5. What operators give us a boolean result?
+
+    // true or false 
+
+//  6.   Declare a variable with the value of "Hello". Covert the value to upper case and print the converted value to the console.
+
+let D = 'Hello';
+console.log(D.toUpperCase());
+
+// 7. Declare another variable with the value of "World". Convert the value to lower case and print the converted value to the console.
+
+let E = 'World';
+console.log(E.toLowerCase());
+
+// 8.  HELLO 
+
+// 9.  undefined 
+
+// 10. hello 
+
+// 11.
+// let age = prompt('How old are you?');
+// let nextAge = parseInt(age) + 1;
+// console.log(`Next year you will be ${nextAge}`);
+
+// 12.
+// let name = prompt('What is your name?');
+// let age = Number(prompt('How old are you?'));
+// let isInSixties = age >= 60 && age < 70;
+// console.log(`${name} is in sixties: ${isInSixties}`);
+
+// 13.    6  
+
+// 14.    6
+
+15
